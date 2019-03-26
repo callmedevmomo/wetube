009ed8d83fe242cf1d5808d5c6e06f4718a2c48c
momo <callmedevmomo@gmail.com>
2019-03-26 15:38:38 +0900

    Home Controller Part Two

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ export const videos = [
         title:'Video nice',
         description:'This is something I love',
         views: 24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
                 id: 121212,
                 name: "momo",
@@ -18,7 +18,7 @@ export const videos = [
         title:'Video awesome',
         description:'This is something I love',
         views: 24,
-        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
                 id: 121212,
                 name: "momo",
@@ -30,7 +30,7 @@ export const videos = [
             title:'Video super',
             description:'This is something I love',
             views: 24,
-            videoFile:"https://archive.org/details/BigBuckBunny_124",
+            videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
             creator: {
                     id: 121212,
                     name: "momo",
@@ -42,7 +42,7 @@ export const videos = [
                 title:'Video perfect',
                 description:'This is something I love',
                 views: 24,
-                videoFile:"https://archive.org/details/BigBuckBunny_124",
+                videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
                 creator: {
                         id: 121212,
                         name: "momo",
