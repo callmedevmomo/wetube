7cf14a2826a20e3fd73ca11e9b52bd930e91d2fc
momo <callmedevmomo@gmail.com>
2019-04-02 00:04:58 +0900

    with Webpack

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import globalRouter from "./routers/globalRouter";;
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
+
 app.use(helmet());
-app.set('view engine',"pug");
-app.use("/uploads", express.static("uploads"))
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 app.use(localsMiddleware);
 
-app.use(routes.home,globalRouter); 
-app.use(routes.users,userRouter);
-app.use(routes.videos,videoRouter);
-
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
