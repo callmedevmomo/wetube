31c85dad46463b8c161f880f97df2d1d801f8815
momo <callmedevmomo@gmail.com>
2019-04-12 17:24:40 +0900

    Custom Video Player --ing

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,8 +97,9 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const getMe = (req, res) => {
-  res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+export const getMe = async (req, res) => {
+  const user = await User.findById(req.user.id).populate("videos");
+  res.render("userDetail", { pageTitle: "userDetail", user });
 };
 
 export const userDetail = async (req, res) => {
@@ -106,8 +107,8 @@ export const userDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const user = await User.findById(id);
-    console.log(user.avatarUrl);
+    const user = await User.findById(id).populate("videos");
+    console.log(user);
     res.render("userDetail", { pageTitle: "User Detail", user });
   } catch (error) {
     res.redirect(routes.home);
